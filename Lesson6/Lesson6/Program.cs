07a27b5ef473bd508da9f5500b66bbc8fc7585d8
Vahe <40205554+RWC18@users.noreply.github.com>
2019-03-05 18:58:37 +0400

    Add files via upload

---
+++ Lesson6/Lesson6/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Lesson6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*                    Random numbers
+                    C#um patahakan tver generacnelu hhamar nax haytararum enq RANDOM dasi object. 
+
+                    1. rd.Next() [0, 4milion]
+                    2. rd.Next(a) [0, a-1]
+                    3. rd.Next(a,b) [a, b-1]
+                    4. rd.NextDouble() [0, 0.9]
+
+            `           ReadKey()
+                    kardum e keyboardi kochakneri infon
+                    .key y parunakum e xosakkcakan anuny
+                    vory haytararvac e consoleKey tvarkman mech, ConsoleKey-y maximal keyboardi  anunneri ev koderi tvarkumn e
+                                                   
+             */
+
+            Random rand = new Random();
+            
+           start:  WriteLine("Enter players names");
+            string p1n = ReadLine();
+            string p2n = ReadLine();
+            int p1_first = rand.Next(1, 7);
+            int p1_second = rand.Next(1, 7);
+            int p2_first = rand.Next(1, 7);
+            int p2_second = rand.Next(1, 7);
+            ForegroundColor = ConsoleColor.Red;
+            WriteLine(p1n + "'s numbers : " + p1_first+" | "+p1_second);
+            ForegroundColor = ConsoleColor.Yellow;
+            WriteLine(p2n + "'s numbers : " + p2_first + " | " + p2_second + "\n");
+            int p1 = p1_first + p1_second;
+            int p2 = p2_first + p2_second;
+
+            if (p1 > p2)
+            {
+                ForegroundColor = ConsoleColor.Red;
+                WriteLine("Wow great "+p1n + " you are win !!! \n");
+                ForegroundColor = ConsoleColor.White;
+
+            }
+            else if (p2 > p1)
+            {
+                ForegroundColor = ConsoleColor.Yellow;
+                WriteLine("Wow great " + p2n + " you are win !!! \n");
+                ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                ForegroundColor = ConsoleColor.White;
+                WriteLine("Draw !!! \n");
+            }
+            
+            WriteLine("You can play again, click on ENTER if you wont to play \n");
+            
+            if (ReadKey().Key == ConsoleKey.Enter)
+            {
+                goto start;
+            }
+
+
+        }
+    }
+}
---
+++ ZarName/ZarName/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ZarName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+              C#um i  tarberutyun C++ karox e linel naev string ev yuraqanchyur Case-i tak partadir petq e lini kam goto
+
+            Ashxatanqi algoritmy
+                1. hashvum e parametri arjeqy
+                2. Cragiry mtnum e voronman rejim, ev sksum e stacvac arjeqy hamematel case-eri het` sksac arajin Case-ic\
+                    a. ete arjeqy havasar che hastatunin, apa voronumy sharunakum e hajord case-um.
+                    b. ete havasar e durs e galis voronman rejimic ev sksum e katarel cragiry sovorakan dzevov.
+                    g. Ete voch mekin havasar che katarvum e defauletum grvac kody ankax nra dirqic.
+                    d. defaulty partadir CASE che.
+
+
+            BREAK Operators
+            
+            break;
+            continue;
+            return;
+            return artahaytutyun;
+
+
+            HomeWork
+                Usery mutq e anum Binar tvabanakan artahaytutyun (5+5), cragiry ogtagorcelov Switch operatory hashvum ev cucadrum e ardyunqy:
+                
+            writeLine("Enter binary Ariphmetic expression");
+            double n1 = double.Parse(ReadLine());
+            char g = char.Parse(ReadLine());
+            double n2 = double.Parse(ReadLine());
+            double result = 0;
+
+             */
+            Random rn = new Random();
+            start: int number = rn.Next(1, 7);
+            
+            switch (number)
+            {
+                case 1:
+                    WriteLine(number + " | Yek");
+                    break;
+                case 2:
+                    WriteLine(number + " | Du");
+                    break;
+                case 3:
+                    WriteLine(number + " | Se");
+                    break;
+                case 4:
+                    WriteLine(number + " | Jar");
+                    break;
+                case 5:
+                    WriteLine(number + " | Panj");
+                    break;
+                case 6:
+                    WriteLine(number + " | Shesh");
+                    break;
+            }
+
+            WriteLine("\n Try again?? press Enter \n");
+            if (ReadKey().Key == ConsoleKey.Enter)
+            {
+                goto start;
+            }
+            
+        }
+    }
+}
